fc031f6e3c7628ad578b505bcc4c5efa148cb596
Ayush Amin <ayush.amin2022@vitstudent.ac.in>
2024-06-02 09:59:16 +0530

    Add setup file and folder structure updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Ayush Amin',
+    author_email= 'ayushnamin@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
